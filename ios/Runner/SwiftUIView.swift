e092c72c72a38a657c69dad3f2d4ef6308300fd9
Vashist Agarwalla <vashist1202@gmail.com>
2023-12-15 19:10:25 +0530

    AppDelegate setup

---
+++ ios/Runner/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Runner
+//
+//  Created by Vashist Agarwalla on 15/12/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
